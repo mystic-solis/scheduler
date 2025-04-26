0640396d4d01690f9eafa61444f138c63242f186
mystic <87268440+mystic-void@users.noreply.github.com>
2025-04-26 17:44:16 +0500

    Подготовил проект к сборке

--- utils/validators.py
+++
@@ -1,9 +0,0 @@
-def check_names_unique(tasks):
-    tasks_set = {task['name'] for task in tasks}
-    tasks_list = [task['name'] for task in tasks]
-
-    tasks_set_len = len(tasks_set)
-    tasks_list_len = len(tasks_list)
-    
-    if tasks_set_len != tasks_list_len:
-        raise Exception('Имена задач должны быть уникальными.')
